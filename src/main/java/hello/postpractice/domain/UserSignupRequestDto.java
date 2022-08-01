9468cc9f080943503500dbee01ea05870bce40e4
lostcatbox <lostcatbox@gmail.com>
2022-08-01 18:36:53 +0900

    signup-vue&logicupdate

--- src/main/java/hello/postpractice/domain/UserSignupRequestDto.java
+++ src/main/java/hello/postpractice/domain/UserSignupRequestDto.java
@@ -3,20 +3,19 @@ package hello.postpractice.domain;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.management.relation.Role;
 import java.util.Collections;
 
 @Getter
 public class UserSignupRequestDto {
     private String email;
     private String password;
-    private String username;
     private String nickname;
 
     @Builder
-    public UserSignupRequestDto(String email, String password, String username, String nickname) {
+    public UserSignupRequestDto(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
-        this.username = username;
         this.nickname = nickname;
     }
 
@@ -25,7 +24,6 @@ public class UserSignupRequestDto {
                 .email(email)
                 .password(password)
                 .nickname(nickname)
-                .username(username)
                 .roles(Collections.singletonList("ROLE_USER"))
                 .build();
     }
