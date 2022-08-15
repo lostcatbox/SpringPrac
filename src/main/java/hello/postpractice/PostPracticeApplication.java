316286dee53f06621f0c2abdfe35b3dee1b4f644
lostcatbox <lostcatbox@gmail.com>
2022-08-15 16:38:01 +0900

    LoggingusingAop

--- src/main/java/hello/postpractice/PostPracticeApplication.java
+++ src/main/java/hello/postpractice/PostPracticeApplication.java
@@ -2,10 +2,12 @@ package hello.postpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAspectJAutoProxy //AspectJ 를 위한 추가
 public class PostPracticeApplication {
 
 	public static void main(String[] args) {
