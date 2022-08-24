6cd1a17e857159dac28e8c9dae9b5ef9f09a95e7
lostcatbox <lostcatbox@gmail.com>
2022-08-24 14:46:22 +0900

    updateLogoutLogic

---
+++ src/main/java/hello/postpractice/config/MyLogoutSuccessHandler.java
@@ -0,0 +1,19 @@
+package hello.postpractice.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class MyLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "*"); //cors이슈 위한설정
+    }
+}
